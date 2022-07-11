c3ed52e2534ecaff1b0da74703b3e793948258e4
Shane <shanemonck11@hotmail.com>
2022-07-11 18:35:00 +0800

    Changed home page and added server

--- TheResistanceOnline.Web/Controllers/WeatherForecastController.cs
+++ TheResistanceOnline.Web/Controllers/WeatherForecastController.cs
@@ -4,12 +4,12 @@ namespace TheResistanceOnline.Web.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController: ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        private static readonly string[] _summaries = new[]
+                                                      {
+                                                          "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+                                                      };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -22,12 +22,12 @@ namespace TheResistanceOnline.Web.Controllers
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+                                                          {
+                                                              Date = DateTime.Now.AddDays(index),
+                                                              TemperatureC = Random.Shared.Next(-20, 55),
+                                                              Summary = _summaries[Random.Shared.Next(_summaries.Length)]
+                                                          })
+                             .ToArray();
         }
     }
 }
