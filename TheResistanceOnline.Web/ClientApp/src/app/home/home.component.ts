e0ee6c8c844979f7d49847a07f210ae6c74c80f6
Shane <shanemonck11@hotmail.com>
2022-07-14 16:55:00 +0800

    fixed not playing issue - bit hacky

--- TheResistanceOnline.Web/ClientApp/src/app/home/home.component.ts
+++ TheResistanceOnline.Web/ClientApp/src/app/home/home.component.ts
@@ -1,16 +1,46 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-})
-export class HomeComponent implements OnInit{
+             selector: 'app-home',
+             templateUrl: './home.component.html'
+           })
+export class HomeComponent implements OnInit {
   public showBackGround1 = true;
   public showBackGround2 = false;
 
+  public videoPlayer1!: HTMLVideoElement;
+  public videoPlayer2!: HTMLVideoElement;
+
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    if(this.showBackGround1) {
+      this.videoPlayer1.muted = true;
+      this.videoPlayer1.play();
+    }
+    if(this.showBackGround2) {
+      this.videoPlayer2.muted = true;
+      this.videoPlayer2.play();
+    }
+  }
+
+  @ViewChild('Video')
+  set mainVideoEl1(el: ElementRef) {
+    if(this.showBackGround1) {
+      this.videoPlayer1 = el.nativeElement;
+    }
+  }
+
+  @ViewChild('Video2')
+  set mainVideoEl2(el: ElementRef) {
+    if(this.showBackGround2) {
+      this.videoPlayer2 = el.nativeElement;
+    }
+  }
 
   constructor() {
+
   }
+
   async ngOnInit(): Promise<void> {
     await this.delay(18760);
     this.showBackGround2 = true;
@@ -19,8 +49,8 @@ export class HomeComponent implements OnInit{
     this.showBackGround1 = false;
   }
 
-   public delay(ms: number) {
-    return new Promise( resolve => setTimeout(resolve, ms) );
+  public delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
   }
 
 }
