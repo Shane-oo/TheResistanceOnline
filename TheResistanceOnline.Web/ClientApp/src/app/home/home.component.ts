f40597858591d6de1a5880bf69638ce506000ded
Shane <shanemonck11@hotmail.com>
2022-07-14 16:05:04 +0800

    Created Cool background and added font

--- TheResistanceOnline.Web/ClientApp/src/app/home/home.component.ts
+++ TheResistanceOnline.Web/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  public showBackGround1 = true;
+  public showBackGround2 = false;
+
+
+  constructor() {
+  }
+  async ngOnInit(): Promise<void> {
+    await this.delay(18760);
+    this.showBackGround2 = true;
+
+    await this.delay(5000);
+    this.showBackGround1 = false;
+  }
+
+   public delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
 }
