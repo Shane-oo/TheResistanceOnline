ad347057e102b8904766b80392ae9746dcca1464
Shane <shanemonck11@hotmail.com>
2022-08-13 18:36:06 +0800

    Update nav-menu.component.ts

--- TheResistanceOnline.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ TheResistanceOnline.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -38,9 +38,6 @@ export class NavMenuComponent implements OnInit {
   logout(){
     this.authService.logout();
     this.router.navigate(['/user']).then(r => {
-      // this.swalService.showSwal(
-      //   'Successfully logged out',
-      //   SwalTypesModel.Success);
     });
   }
 
