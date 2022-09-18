8193d9a854117d44b3d2255c4bec1137449497b6
Shane <shanemonck11@hotmail.com>
2022-09-18 15:12:27 +0800

    Finished Nav Bar very happy

---
+++ TheResistanceOnline.Web/ClientApp/src/app/nav-menu/nav-data.ts
@@ -0,0 +1,42 @@
+export const navbarDataLoggedIn = [
+  {
+    // Home Component
+    routeLink: '',
+    icon: 'home',
+    label: 'Home'
+  },
+  {
+    // Game Component
+    routeLink: 'the-resistance-game',
+    icon: 'videogame_asset',
+    label: 'Game'
+  },
+  //Account Component
+  {
+    routeLink: 'user/user-edit',
+    icon: 'manage_accounts',
+    label: 'Account'
+  }
+];
+
+export const navbarDataNotLoggedIn = [
+  {
+    // Home Component
+    routeLink: '',
+    icon: 'home',
+    label: 'Home'
+  },
+  {
+    // Login Component
+    routeLink: 'user',
+    icon: 'login',
+    label: 'Login'
+  }
+];
+
+//todo put somewhere better
+export interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
+
--- TheResistanceOnline.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ TheResistanceOnline.Web/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,16 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, HostListener, OnInit, Output } from '@angular/core';
 import { AuthenticationService } from '../user/authentication.service';
 import { Router } from '@angular/router';
+import { navbarDataLoggedIn, navbarDataNotLoggedIn, SideNavToggle } from './nav-data';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
              selector: 'app-nav-menu',
              templateUrl: './nav-menu.component.html',
-             styleUrls: ['./nav-menu.component.css']
+             styleUrls: ['./nav-menu.component.css'],
+             animations: [
+               trigger('fadeInOut', [
+                 transition(':enter', [
+                   style({opacity: 0}),
+                   animate('350ms',
+                           style({
+                                   opacity: 1
+                                 }))
+                 ]),
+                 transition(':leave', [
+                   style({opacity: 1}),
+                   animate('350ms',
+                           style({
+                                   opacity: 0
+                                 }))
+                 ])
+               ])
+             ]
            })
 export class NavMenuComponent implements OnInit {
-  isExpanded = false;
   isUserAuthenticated = false;
   isUserAdmin = false;
+
+  @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter<SideNavToggle>();
+  collapsed = false;
+  screenWidth = 0;
+  navDataLoggedIn = navbarDataLoggedIn;
+  navDataNotLoggedIn = navbarDataNotLoggedIn;
 
   constructor(private authService: AuthenticationService, private router: Router) {
     this.authService.authChanged.subscribe(res => {
@@ -21,17 +46,18 @@ export class NavMenuComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
+    this.screenWidth = window.innerWidth;
   }
 
-
-  collapse() {
-    this.isExpanded = false;
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth <= 768) {
+      this.collapsed = false;
+      this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+    }
   }
 
-  toggle() {
-    this.isExpanded = !this.isExpanded;
-  }
 
   logout() {
     this.authService.logout();
@@ -39,4 +65,14 @@ export class NavMenuComponent implements OnInit {
     });
   }
 
+  toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+
+  }
+
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+  }
 }
