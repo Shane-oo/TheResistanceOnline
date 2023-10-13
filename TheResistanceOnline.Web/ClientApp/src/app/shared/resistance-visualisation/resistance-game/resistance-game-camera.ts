7d76e6e5396f56311f8676dcce992289eef0c303
Shane <shanemonck11@hotmail.com>
2023-10-13 15:57:23 +0800

    #184 add resize timeout

--- TheResistanceOnline.Web/ClientApp/src/app/shared/resistance-visualisation/resistance-game/resistance-game-camera.ts
+++ TheResistanceOnline.Web/ClientApp/src/app/shared/resistance-visualisation/resistance-game/resistance-game-camera.ts
@@ -6,108 +6,108 @@ import GUI from "lil-gui";
 
 export class ResistanceGameCamera {
 
-    // Values
-    private readonly fov = 32;
-    private readonly planeAspectRatio = 2.75;
+  // Values
+  private readonly fov = 32;
+  private readonly planeAspectRatio = 2.75;
 
-    private readonly _perspectiveCamera: PerspectiveCamera;
-    private readonly sizes: Sizes;
-    private readonly orbitControls: OrbitControls;
-    private readonly scene: Scene;
-    private readonly canvas: HTMLCanvasElement;
+  private readonly _perspectiveCamera: PerspectiveCamera;
+  private readonly sizes: Sizes;
+  private readonly orbitControls: OrbitControls;
+  private readonly scene: Scene;
+  private readonly canvas: HTMLCanvasElement;
 
-    // Debug
-    private readonly debugFolder?: GUI;
+  // Debug
+  private readonly debugFolder?: GUI;
 
-    constructor(scene: Scene, sizes: Sizes, canvas: HTMLCanvasElement, debug: Debug) {
-        this.sizes = sizes;
-        this.scene = scene;
-        this.canvas = canvas;
-          console.log(this.sizes.width)
-        console.log(this.sizes.height)
-        // Perspective Camera
-        this._perspectiveCamera = new PerspectiveCamera(this.fov,
-            this.sizes.width / this.sizes.height,
-            0.1,
-            100);
-        this.configureCamera();
-        this.scene.add(this._perspectiveCamera);
+  constructor(scene: Scene, sizes: Sizes, canvas: HTMLCanvasElement, debug: Debug) {
+    this.sizes = sizes;
+    this.scene = scene;
+    this.canvas = canvas;
+    console.log(this.sizes.width)
+    console.log(this.sizes.height)
+    // Perspective Camera
+    this._perspectiveCamera = new PerspectiveCamera(this.fov,
+      this.sizes.width / this.sizes.height,
+      0.1,
+      100);
+    this.configureCamera();
+    this.scene.add(this._perspectiveCamera);
 
-        // Orbit Controls
-        this.orbitControls = new OrbitControls(this._perspectiveCamera, this.canvas);
-        this.orbitControls.enableDamping = true;
-        this.orbitControls.enabled = false;
-
-        // Debug
-        if (debug.gui) {
-            this.debugFolder = debug.gui.addFolder('Camera');
-            this.configureDebug();
-        }
-    }
-
-    get perspectiveCamera(): PerspectiveCamera {
-        return this._perspectiveCamera;
-    }
-
-    resize() {
-        this._perspectiveCamera.aspect = this.sizes.width / this.sizes.height;
-
-        if (this._perspectiveCamera.aspect > this.planeAspectRatio) {
-            // window too large
-            this._perspectiveCamera.fov = this.fov;
-        } else {
-            // window too narrow
-            const cameraHeight = Math.tan(MathUtils.degToRad(this.fov / 2));
-            const ratio = this._perspectiveCamera.aspect / this.planeAspectRatio;
-            const newCameraHeight = cameraHeight / ratio;
-            this._perspectiveCamera.fov = MathUtils.radToDeg(Math.atan(newCameraHeight)) * 2;
-        }
-
-
-        this._perspectiveCamera.updateProjectionMatrix();
-    }
-
-    update() {
-        this.orbitControls.update();
-
-    }
-
-    destroy() {
-        this.scene.remove(this._perspectiveCamera);
-        this.orbitControls.dispose();
-    }
-
-    private configureCamera() {
-        this._perspectiveCamera.position.set(0, 5.2, 0);
-        this.resize();
-    }
+    // Orbit Controls
+    this.orbitControls = new OrbitControls(this._perspectiveCamera, this.canvas);
+    this.orbitControls.enableDamping = true;
+    this.orbitControls.enabled = false;
 
     // Debug
-    private configureDebug() {
-        if (this.debugFolder) {
-            this.debugFolder.add(this._perspectiveCamera, 'fov')
-                .name('fov')
-                .min(-1000)
-                .max(1000)
-                .step(0.001)
-                .onChange(() => this._perspectiveCamera.updateProjectionMatrix());
-
-            this.debugFolder.add(this._perspectiveCamera.position, 'x')
-                .name('cameraX')
-                .min(-100)
-                .max(100)
-                .step(0.001);
-            this.debugFolder.add(this._perspectiveCamera.position, 'y')
-                .name('cameraY')
-                .min(-100)
-                .max(100)
-                .step(0.001);
-            this.debugFolder.add(this._perspectiveCamera.position, 'z')
-                .name('cameraZ')
-                .min(-100)
-                .max(100)
-                .step(0.001);
-        }
-
+    if (debug.gui) {
+      this.debugFolder = debug.gui.addFolder('Camera');
+      this.configureDebug();
     }
+  }
+
+  get perspectiveCamera(): PerspectiveCamera {
+    return this._perspectiveCamera;
+  }
+
+  resize() {
+    this._perspectiveCamera.aspect = this.sizes.width / this.sizes.height;
+
+    if (this._perspectiveCamera.aspect > this.planeAspectRatio) {
+      // window too large
+      this._perspectiveCamera.fov = this.fov;
+    } else {
+      // window too narrow
+      const cameraHeight = Math.tan(MathUtils.degToRad(this.fov / 2));
+      const ratio = this._perspectiveCamera.aspect / this.planeAspectRatio;
+      const newCameraHeight = cameraHeight / ratio;
+      this._perspectiveCamera.fov = MathUtils.radToDeg(Math.atan(newCameraHeight)) * 2;
+    }
+
+
+    this._perspectiveCamera.updateProjectionMatrix();
+  }
+
+  update() {
+    this.orbitControls.update();
+
+  }
+
+  destroy() {
+    this.scene.remove(this._perspectiveCamera);
+    this.orbitControls.dispose();
+  }
+
+  private configureCamera() {
+    this._perspectiveCamera.position.set(0, 5.2, 0);
+    this.resize();
+  }
+
+  // Debug
+  private configureDebug() {
+    if (this.debugFolder) {
+      this.debugFolder.add(this._perspectiveCamera, 'fov')
+        .name('fov')
+        .min(-1000)
+        .max(1000)
+        .step(0.001)
+        .onChange(() => this._perspectiveCamera.updateProjectionMatrix());
+
+      this.debugFolder.add(this._perspectiveCamera.position, 'x')
+        .name('cameraX')
+        .min(-100)
+        .max(100)
+        .step(0.001);
+      this.debugFolder.add(this._perspectiveCamera.position, 'y')
+        .name('cameraY')
+        .min(-100)
+        .max(100)
+        .step(0.001);
+      this.debugFolder.add(this._perspectiveCamera.position, 'z')
+        .name('cameraZ')
+        .min(-100)
+        .max(100)
+        .step(0.001);
+    }
+
+  }
 }
--- TheResistanceOnline.Web/ClientApp/src/app/shared/resistance-visualisation/resistance-game/utils/sizes.ts
+++ TheResistanceOnline.Web/ClientApp/src/app/shared/resistance-visualisation/resistance-game/utils/sizes.ts
@@ -1,47 +1,55 @@
 import {Subject} from "rxjs";
 
 export class Sizes {
-    resizeSubject: Subject<void> = new Subject<void>();
-    private readonly widthOffset = 1.33;
-    private readonly heightOffset = 1.66;
+  resizeSubject: Subject<void> = new Subject<void>();
+  private readonly widthOffset = 1.33;
+  private readonly heightOffset = 1.66;
+  private resizeTimeout: any;
 
-    constructor() {
-        // Setup
-        this._width = window.innerWidth / this.widthOffset;
-        this._height = window.innerHeight / this.heightOffset;
-        this._pixelRatio = Math.min(window.devicePixelRatio, 2);
+  constructor() {
+    // Setup
+    this._width = window.innerWidth / this.widthOffset;
+    this._height = window.innerHeight / this.heightOffset;
+    this._pixelRatio = Math.min(window.devicePixelRatio, 2);
 
-        // Resize event
-        window.addEventListener('resize', this.resize);
+    // Resize event
+    window.addEventListener('resize', this.resize);
+  }
+
+  private _height: number;
+
+  get height(): number {
+    return this._height;
+  }
+
+  private _pixelRatio: number;
+
+  get pixelRatio(): number {
+    return this._pixelRatio;
+  }
+
+  private _width: number;
+
+  get width(): number {
+    return this._width;
+  }
+
+  destroy() {
+    window.removeEventListener('resize', this.resize);
+  }
+
+  private resize = () => {
+    if (this.resizeTimeout) {
+      clearTimeout(this.resizeTimeout);
     }
 
-    private _height: number;
+    this.resizeTimeout = setTimeout(() => {
+      this._width = window.innerWidth / this.widthOffset;
+      this._height = window.innerHeight / this.heightOffset;
+      this._pixelRatio = Math.min(window.devicePixelRatio, 2);
+      console.log(this.width)
+      this.resizeSubject.next();
+    }, 100)
 
-    get height(): number {
-        return this._height;
-    }
-
-    private _pixelRatio: number;
-
-    get pixelRatio(): number {
-        return this._pixelRatio;
-    }
-
-    private _width: number;
-
-    get width(): number {
-        return this._width;
-    }
-
-    destroy() {
-        window.removeEventListener('resize', this.resize);
-    }
-
-    private resize = () => {
-        this._width = window.innerWidth / this.widthOffset;
-        this._height = window.innerHeight / this.heightOffset;
-        this._pixelRatio = Math.min(window.devicePixelRatio, 2);
-
-        this.resizeSubject.next();
-    }
+  }
 }
